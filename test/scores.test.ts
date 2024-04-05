d24f22c44f73954398cbcd4cc0d1bcb20af198b2
James Knight <tech@jamesknightcoaching.com>
2024-04-05 15:53:26 +0100

    Add test placeholders

---
+++ test/scores.test.ts
@@ -0,0 +1,16 @@
+// test/index.test.ts
+import { afterAll, describe, expect, it } from 'bun:test'
+import { app } from "../src/index";
+
+
+const baseUrl = `${app.server?.hostname}:${app.server?.port}/api`;
+
+describe('SCORES Test suite', () => {
+    describe('GET SCORES Test suite', () => {
+        it('should return a list of scores successfully', async () => {
+            const req = new Request(`${baseUrl}/scores`);
+            const res = await app.fetch(req);
+            expect(res.status).toEqual(200);
+        });
+    });
+});
