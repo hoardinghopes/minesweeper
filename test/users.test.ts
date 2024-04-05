266a3dbbb9b91dcd1f1814d5bdcded7307b82247
James Knight <tech@jamesknightcoaching.com>
2024-04-05 17:32:07 +0100

    New test, based on test.db

--- test/users.test.ts
+++ test/users.test.ts
@@ -8,24 +8,22 @@ const baseUrl = `http://${app.server?.hostname}:${app.server?.port}/api`;
 describe('USERS Test suite', () => {
     describe('GET USERS Test suite', () => {
 
-        it('should return a list of users successfully', async () => {
+        it('should return 200', async () => {
             const req = new Request(`${baseUrl}/users`);
             const res = await app.fetch(req);
             expect(res.status).toEqual(200);
         });
 
 
-        // it('return a response', async () => {
+        it('return a response', async () => {
 
+            const response = await app
+                .handle(new Request(`${baseUrl}/users`))
+                .then((res) => res.text())
 
-        //     const response = await app
-        //         .handle(new Request(`${baseUrl}/users`))
-        //         .then((res) => res.text())
-
-        //     console.log(response);
-        //     expect(response).toBe('[{"id":1,"name":"jamesk","createdAt":"2024-04-04T16:58:45.000Z"},{"id":2,"name":"mightymai","createdAt":"2024-04-04T16:58:52.000Z"},{"id":3,"name":"shonaVK","createdAt":"2024-04-04T16:58:57.000Z"},{"id":6,"name":"another","createdAt":"2024-04-05T13:46:05.000Z"}]')
-        // })
+            console.log(response);
+            expect(response).toBe('[{"id":1,"name":"test","createdAt":"2024-04-04T16:58:45.000Z"},{"id":2,"name":"mightymai","createdAt":"2024-04-04T16:58:52.000Z"},{"id":3,"name":"shonaVK","createdAt":"2024-04-04T16:58:57.000Z"},{"id":6,"name":"another","createdAt":"2024-04-05T13:46:05.000Z"}]')
+        })
     });
-
 
 });
